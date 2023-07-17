9102737d6a95af438e8a8030ddfbcbe1b64bfcf5
unknown <janoncarlos@gmail.com>
2023-07-17 16:36:59 -0300

    heroes by id

--- src/heroes/pages/Hero.jsx
+++ src/heroes/pages/Hero.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router'
+import { getHeroById } from '../helpers/getHeroById'
 
 const Hero = () => {
+  const { id } = useParams()
+  const hero = getHeroById(id)
+  console.log(hero);
   return (
-    <h1>Hero</h1>
+    <h1>Hero {id}</h1>
   )
 }
 
