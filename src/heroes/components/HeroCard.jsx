2eddc301b305eef5b8d11674eb426376a7cf8ae2
unknown <janoncarlos@gmail.com>
2023-07-17 16:13:13 -0300

    heroes card

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
 
@@ -6,22 +7,21 @@ const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, chara
   return (
     <div className='col'>
       <div className='card'>
-
         <div className='row no-gutters'>
           <div className='col-4'>
-
             <img src={heroImageUrl} alt={superhero} className='card-img' />
-
-
           </div>
-
+          <div className='col-8'>
+            <div className='card-body'>
+              <h5 className='card-title'>{superhero}</h5>
+              <p>{alter_ego}</p>
+              {(characters !== alter_ego) && <p>{characters}</p>}
+            </div>
+            <p className='card-text'> <small className='text-muted'>{first_appearance}</small> </p>
+            <Link to={`/hero/${id}`}> More info...</Link>
+          </div>
         </div>
-
-
-
       </div>
-
-
     </div>
   )
 }
--- src/heroes/components/HeroeList.jsx
+++ src/heroes/components/HeroeList.jsx
@@ -6,7 +6,7 @@ const HeroeList = (publisher) => {
   const heroes = getHeroesByPublisher(publisher)
 
   return (
-    <div className='row rows-cols-1 row-cols-md-3 g-3' >
+    <div className='row rows-cols-1 row-cols-md-2 g-3' >
       {heroes.map(hero => (<HeroCard key={hero.id} {...hero} />))}
 
     </div>
