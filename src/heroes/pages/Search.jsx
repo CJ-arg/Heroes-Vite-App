52bf47a8da9a1391d5690424634acc0fd7db503c
unknown <janoncarlos@gmail.com>
2023-07-18 10:34:40 -0300

    search completed

--- src/heroes/pages/Search.jsx
+++ src/heroes/pages/Search.jsx
@@ -8,20 +8,16 @@ import { getHeroesByName } from '../helpers/getHeroesByName'
 const Search = () => {
   const navigate = useNavigate()
   const location = useLocation()
-
   const { q = '' } = queryString.parse(location.search)
   const heroes = getHeroesByName(q)
   const { searchText, onInputChange } = useForm({
     searchText: q
   })
-
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if (searchText.trim().length <= 1) return;
+    // if (searchText.trim().length <= 1) return;
     navigate(`?q=${searchText}`);
   }
-
-
   return (
     <>
       <h1>Search</h1>
@@ -45,9 +41,9 @@ const Search = () => {
         <div className='col-7'>
           <h4>Results</h4>
           <hr />
+          {(q === '') ? <div className='alert alert-primary'> Search a Hero</div> : (heroes.length === 0) && <div className='alert alert-danger'> No results for that input <b>{q}</b> </div>}
 
-          <div className='alert alert-primary'> Search a Hero</div>
-          <div className='alert alert-danger'> No results for that input <b>{q}</b> </div>
+
           {heroes.map(hero => (<HeroCard key={hero.id} {...hero} />))}
 
         </div>
