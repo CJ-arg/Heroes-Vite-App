d91f08b1cfdb1cc87a5abab132c1dfce05251c42
unknown <janoncarlos@gmail.com>
2023-07-18 10:36:53 -0300

    search completed

--- src/heroes/pages/Search.jsx
+++ src/heroes/pages/Search.jsx
@@ -41,7 +41,7 @@ const Search = () => {
         <div className='col-7'>
           <h4>Results</h4>
           <hr />
-          {(q === '') ? <div className='alert alert-primary'> Search a Hero</div> : (heroes.length === 0) && <div className='alert alert-danger'> No results for that input <b>{q}</b> </div>}
+          {(q === '') ? <div className='alert alert-primary animate__animated animate__fadeIn'> Search a Hero</div> : (heroes.length === 0) && <div className='alert alert-danger animate__animated animate__fadeIn'> No results for that input <b>{q}</b> </div>}
 
 
           {heroes.map(hero => (<HeroCard key={hero.id} {...hero} />))}
